02255243714935aaec5670931e4bdbff1f6df1dd
ArachnophobicSpyder <arachnophobicspyder@gmail.com>
2026-01-27 20:11:44 +1100

    Player and Input Changes
    - Made SPEED var @export pretty much the [SerializeField] of Godot (shows in inspector and saves)
    - Added up and down and made inputs custom actions (actions for keyboard, gampad stick/dpad)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+@export var SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Custom gameplay actions set in Project > Project Settings > Input Map.
+	var direction := Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED) #Vector2 version of the function
 
 	move_and_slide()
