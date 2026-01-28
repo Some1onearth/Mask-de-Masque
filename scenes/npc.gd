00e4031c3edef5431f8bbf2a91f3e28b1f9e3d54
dinopoke <dinopoke@hotmail.com>
2026-01-28 17:15:08 +1100

    Tried to do the throne room...

--- scenes/npc.gd
+++ scenes/npc.gd
@@ -6,12 +6,14 @@ var is_chatting = false
 var player_in_area = false
 
 func _ready():
-	pass
+	Dialogic.signal_event.connect(DialogicSignal)
 	
 func _process(delta):
 	if player_in_area:
 		if Input.is_action_just_pressed("dialogic_default_action"):
-			run_dialogue("test_timeline")
+			print(is_chatting)
+			if is_chatting == false:
+				run_dialogue("test_timeline")
 
 func _on_chat_detection_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
@@ -24,5 +26,9 @@ func _on_chat_detection_body_exited(body: Node2D) -> void:
 
 func run_dialogue(dialogue_string):
 	is_chatting = true
-	
 	Dialogic.start(dialogue_string)
+
+	
+func DialogicSignal(arg: String):
+	if arg == "exit":
+		is_chatting = false
