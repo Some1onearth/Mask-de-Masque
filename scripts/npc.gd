1b1e925aff5d96e29f5f816164d2b8dd433d1dd8
ArachnophobicSpyder <arachnophobicspyder@gmail.com>
2026-01-29 20:44:36 +1100

    Uhhh a lot (sorry)
    - Tried to organise things into folders somewhat
    - Added audio files
    - Dialogic stuff for cellar keeper and peasant
    - Player's mask mechanic started
    - NPCs now need/use name of conversation assigned in inspector

---
+++ scripts/npc.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+##Name of Dialogic timeline for character dialogue (don't add file extension)
+@export var dialogue_timeline_name:String
+
+var is_chatting = false
+
+var player_in_area = false
+
+func _ready():
+	Dialogic.signal_event.connect(DialogicSignal)
+	
+func _process(delta):
+	if player_in_area:
+		if Input.is_action_just_pressed("dialogic_default_action"):
+			#print("is_chatting: %s" % is_chatting)
+			if is_chatting == false:
+				run_dialogue(dialogue_timeline_name)
+
+func _on_chat_detection_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
+
+
+func _on_chat_detection_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false
+
+func run_dialogue(dialogue_string):
+	Dialogic.timeline_ended.connect(ended_dialogue)
+	is_chatting = true
+	Dialogic.start(dialogue_string)
+	
+
+func ended_dialogue():
+	Dialogic.timeline_ended.disconnect(ended_dialogue)
+	is_chatting = false
+	
+		
+func DialogicSignal(arg: String):
+	if arg == "exit":
+		is_chatting = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 
+#var masks = ["wild","peasant","eunuch","noble"]
+var current_mask:String = "wild"
+
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# Custom gameplay actions set in Project > Project Settings > Input Map.
@@ -10,8 +13,27 @@ func _physics_process(delta: float) -> void:
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED) #Vector2 version of the function
-
+	
 	move_and_slide()
+	
+	#Mask stuff
+	#PLACEHOLDER INPUT FOR TESTING CHANGE LATER
+	#idk what the mask changing is supposed to be so masks assigned on number keys 1 to 4
+	if Input.is_key_pressed(KEY_1):
+		maskSwap("wild")
+	if Input.is_key_pressed(KEY_2):
+		maskSwap("peasant")
+	if Input.is_key_pressed(KEY_3):
+		maskSwap("eunuch")
+	if Input.is_key_pressed(KEY_4):
+		maskSwap("noble")
+	
+func maskSwap(mask_name:String):
+	current_mask = mask_name
+	#sets the variable in Dialogic
+	Dialogic.VAR.CurrentMask = current_mask
+	print("mask changed to %s" % mask_name)
+	pass
 	
 func player():
 	pass
