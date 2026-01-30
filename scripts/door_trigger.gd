537d0f94c87202ee53ab90e01922a4786d11114f
jae <jemztones@users.noreply.github.com>
2026-01-30 23:41:32 +1100

    Door script ignores collisions from nodes other than the player. Log a message if the player doesn't have enough points

--- scripts/door_trigger.gd
+++ scripts/door_trigger.gd
@@ -5,6 +5,10 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	if not body is Player:
+		return
 	print("SocialPoints: %s" % Dialogic.VAR.SocialPoints)
-	if body is Player and Dialogic.VAR.SocialPoints >= lock_threshold:
+	if Dialogic.VAR.SocialPoints >= lock_threshold:
 		get_tree().change_scene_to_packed(next_scene)
+	else:
+		print("You need %s SocialPoints to enter this room" % lock_threshold)
